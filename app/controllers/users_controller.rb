36fdcdd86ea6229070927715799f2aa5df2241ea
やす <i.20010911@icloud.com>
2023-05-12 13:56:25 +0900

    タイポ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmaiton, :avatar)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
