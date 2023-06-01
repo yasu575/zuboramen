69f9881aa011c5e4c1318fbb8e14bd40beb35c03
やす <i.20010911@icloud.com>
2023-06-01 12:44:16 +0900

    ページネーション機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[edit update destroy]
 
   def index
-    @posts = Post.all.includes(:user).order(created_at: :desc)
+    @posts = Post.all.includes(:user).order(created_at: :desc).page(params[:page])
   end
 
   def new 
